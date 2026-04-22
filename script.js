b8864b870ad41e6e5a989ab55c3a80f7889bc524
JAIME ALFREDO MUÑOZ PEREZ <jaimealfredo.jm2@gmail.com>
2026-04-22 18:02:38 -0500

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const menuToggle =document.querySelector(".menu-toggle");
+const navMenu = document.querySelector(".nav-menu");
+//Abrir y cerrar el menú
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
 
+//Cerrar el menú  al hacer clic afuera
+document.addEventListener("click", (e) => {
+  if (
+    !menuToggle.contains(e.target) &&
+    !navMenu.contains(e.target)
+  ){
+    navMenu.classList.remove("active");
+  }
+});
